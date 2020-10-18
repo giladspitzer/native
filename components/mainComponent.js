e59a572b3f6d14cc8559e2a56d0e3419f854aab1
Gilad Spitzer <giladspitzer@gmail.com>
2020-10-18 20:31:28 +0300

    stack nav

--- components/mainComponent.js
+++ components/mainComponent.js
@@ -1,19 +1,50 @@
+import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import Menu from './menuComponent';
-import { DISHES } from '../shared/dishes';
+import DishDetail from './dishDetailComponent';
+import { View, Platform } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Constants } from 'expo' 
+
+const MenuNavigator = createStackNavigator();
+
+function MyStack(){
+    return(
+        <MenuNavigator.Navigator initialRouteName="Menu" headerMode="screen"
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#f4511e',
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                }
+            }}
+    
+        >
+            <MenuNavigator.Screen
+                name="Menu"
+                component={ Menu }
+            />
+            <MenuNavigator.Screen
+                name="Dish Detail"
+                component={ DishDetail }
+            />
+        </MenuNavigator.Navigator>
+    )
+}
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
 
   render() {
  
     return (
-        <Menu dishes={this.state.dishes} />
+        <View style={{flex:1}}>
+            <NavigationContainer>
+                <MyStack />
+            </NavigationContainer>
+        </View>
     );
   }
 }
